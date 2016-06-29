7a971502025775cafa55d9647ab340d8db93d263
Chris Yallop <chris.yallop@adnams.co.uk>
2016-06-29 23:24:37 +0100

    Adds remaining functionality through web UI

--- src/routes.php
+++ src/routes.php
@@ -7,15 +7,28 @@ $app->get('/[{name}]', function (\Slim\Http\Request $request, $response, $args) 
 
     $sellingPrice   = \App\Domain\Model\Money::fromAmount(50);
     $vendingMachine = \App\Domain\Model\VendingMachine::startWithSellingPrice($sellingPrice);
+    $vendingMachine->setInventory([
+        100  =>  11,
+         50  =>  24,
+         20  =>   0,
+         10  =>  99,
+          5  => 200,
+          2  =>  11,
+          1  =>  23,
+    ]);
+
+    $args['sellingPrice']       = $vendingMachine->getSellingPrice()->getAmount();
+    $args['startingInventory']  = $vendingMachine->getInventory();
 
     $purchaseAmount = $request->getParam('purchaseAmount');
     if ($purchaseAmount) {
         $purchaseAmount = \App\Domain\Model\Money::fromCoin((int) $purchaseAmount);
         $change         = $vendingMachine->purchaseItem($purchaseAmount);
-        $args['change'] = $change;
-    }
 
-    $args['vendingMachine'] = $vendingMachine;
+        $args['purchaseAmount']     = $purchaseAmount->getAmount();
+        $args['change']             = $change;
+        $args['closingInventory']   = $vendingMachine->getInventory();
+    }
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
